da2dca88e44d841241d6028cebdbf1983e694da2
oemurlu <oemurlu@gmail.com>
2022-12-05 23:17:31 +0300

    Initial commit

---
+++ MovieFinder/Controller/DetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  MovieFinder
+//
+//  Created by Osman Emre Ömürlü on 5.12.2022.
+//
+
+import Foundation
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ MovieFinder/Controller/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFinder
+//
+//  Created by Osman Emre Ömürlü on 5.12.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
